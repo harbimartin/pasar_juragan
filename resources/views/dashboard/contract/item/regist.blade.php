17d0099e8cbef2aef98ee30198288013405f12bf
Harbi Martin <harbimartin@gmail.com>
2022-11-04 17:49:03 +0700

    Order Angkutan

--- resources/views/dashboard/contract/item/regist.blade.php
+++ resources/views/dashboard/contract/item/regist.blade.php
@@ -4,7 +4,8 @@
         $column = [
             'juragan_angkutan_id' => ['name' => 'Juragan Angkutan', 'type' => 'Select', 'val' => ['provider_name'], 'api' => 'angkutan', 'full' => true],
             'juragan_barang_id' => ['name' => 'Juragan Barang', 'type' => 'Select', 'val' => ['comp_name'], 'api' => 'barang', 'full' => true],
-            'contract_no' => ['name' => 'Nama', 'type' => 'String', 'full' => true],
+            'contract_no' => ['name' => 'No Kontrak', 'type' => 'String', 'full' => true],
+            'contract_desc' => ['name' => 'Judul Kontrak', 'type' => 'String', 'full' => true],
             'contract_date' => ['name' => 'Tanggal Kontrak', 'type' => 'Date', 'full' => true],
             'contract_expired' => ['name' => 'Tanggal Expired', 'type' => 'Date', 'full' => true],
             'file' => ['name' => 'Upload Lampiran', 'type' => 'Upload', 'accept' => 'application/pdf', 'key' => 'file', 'folder' => 'file_contract', 'full' => true],
